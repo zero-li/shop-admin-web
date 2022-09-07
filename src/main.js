344c2eb881332d8abe01933edd01454a9efbf252
zgo <zero.zhh.li@gmail.com>
2022-09-07 09:48:33 +0800

    config windicss
    https://cn.windicss.org/integrations/vite.html

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'virtual:windi.css'
+
 
 import App from './App.vue'
 
-import './assets/main.css'
 
 const app = createApp(App);
 
