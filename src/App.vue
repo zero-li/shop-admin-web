e1a7f4e0b27e85da876dd93ced188356324035ef
zgo <zero.zhh.li@gmail.com>
2022-09-07 09:58:58 +0800

    apply windicss button

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,14 @@
     <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
   </el-row>
+
+  <button class="btn">按钮</button>
 </template>
 
 
 <style>
+  .btn{
+    @apply bg-purple-500 text-indigo-50 px-4 py-1 rounded-full transition-all duration-500 hover:(bg-purple-900) focus:(ring-8 ring-purple-900)
+  }
 
 </style>
