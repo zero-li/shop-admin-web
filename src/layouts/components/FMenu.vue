90cd60e610ea753a59ec8e730554bedd0072fb58
zgo <zero.li.zhh@gmail.com>
2022-09-13 15:07:19 +0800

    侧边菜单开发-样式布局和路由跳转

--- src/layouts/components/FMenu.vue
+++ src/layouts/components/FMenu.vue
@@ -1,5 +1,89 @@
 <template>
-    <div>
-        侧边菜单
+    <div class="f-menu">
+        <el-menu default-active="2" class="border-0" @select="handleSelect">
+
+            <template v-for="(item, index) in asideMenus" :key="index">
+                <el-sub-menu v-if="item.child && item.child.length>0" :index="item.name">
+                    <template #title>
+                        <el-icon>
+                            <component :is="item.icon"></component>
+                        </el-icon>
+                        <span>{{ item.name}}</span>
+
+                    </template>
+
+                    <el-menu-item v-for="(item2, index2) in item.child" :key="index2" :index="item2.frontpath">
+                        <el-icon>
+                            <component :is="item2.icon"></component>
+                        </el-icon>
+                        <span>{{item2.name}}</span>
+
+                    </el-menu-item>
+                </el-sub-menu>
+
+                <el-sub-menu v-else :index="item.frontpath">
+                    <el-icon>
+                        <component :is="item.icon"></component>
+                    </el-icon>
+                    <span>{{item.name}}</span>
+
+                </el-sub-menu>
+
+            </template>
+
+
+
+
+        </el-menu>
+
     </div>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+
+const router = useRouter()
+
+
+const asideMenus = [{
+    "name": "后台面板",
+    "icon": "help",
+    "child": [{
+        "name": "主控台",
+        "icon": "home-filled",
+        "frontpath": "/",
+    }]
+}, {
+    "name": "商城管理",
+    "icon": "shopping-bag",
+    "child": [{
+        "name": "商品管理",
+        "icon": "shopping-cart-full",
+        "frontpath": "/goods/list",
+    }]
+}]
+
+const handleSelect = (e)=>{
+    router.push(e)
+
+}
+
+
+const handleSelect2= (e)=>{
+    router.push(e)
+}
+
+</script>
+<style>
+.f-menu {
+    width: 240px;
+    top:64px;
+    bottom:0;
+    left:0;
+    overflow: auto;
+    @apply shadow-md fixed bg-light-50;
+
+}
+
+</style>
