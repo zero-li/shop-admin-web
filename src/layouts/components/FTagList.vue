71998214ac560a76f6838f38edbec85b066537b5
zgo <zero.li.zhh@gmail.com>
2022-09-18 10:51:15 +0800

    标签导航组件实现-样式布局

--- src/layouts/components/FTagList.vue
+++ src/layouts/components/FTagList.vue
@@ -1,5 +1,146 @@
 <template>
-    <div>
-        标签导航
+    <div class="f-tag-list" :style="{ left:$store.state.asideWidth }">
+
+        <el-tabs tab-position="bottom" v-model="editableTabsValue" type="card" class="flex-1" closable @tab-remove="removeTab" style="min-width: 100px;">
+            <el-tab-pane v-for="item in editableTabs" :key="item.name" :label="item.title" :name="item.name">
+            </el-tab-pane>
+        </el-tabs>
+
+
+        <span class="tag-btn">
+            <el-dropdown>
+                <span class="el-dropdown-link">
+                    <el-icon>
+                        <arrow-down />
+                    </el-icon>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>Action 1</el-dropdown-item>
+                        <el-dropdown-item>Action 2</el-dropdown-item>
+                        <el-dropdown-item>Action 3</el-dropdown-item>
+                        <el-dropdown-item disabled>Action 4</el-dropdown-item>
+                        <el-dropdown-item divided>Action 5</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </span>
+
     </div>
 </template>
+<script setup>
+import { ref } from 'vue'
+
+let tabIndex = 2
+const editableTabsValue = ref('2')
+const editableTabs = ref([
+    {
+        title: 'Tab 1',
+        name: '1',
+        content: 'Tab 1 content',
+    },
+    {
+        title: 'Tab 2',
+        name: '2',
+        content: 'Tab 2 content',
+    },
+    {
+        title: 'Tab 3',
+        name: '3',
+        content: 'Tab 3 content',
+    },
+    {
+        title: 'Tab 4',
+        name: '4',
+        content: 'Tab 4 content',
+    },
+    {
+        title: 'Tab 5',
+        name: '5',
+        content: 'Tab 5 content',
+    },
+    {
+        title: 'Tab 6',
+        name: '6',
+        content: 'Tab 6 content',
+    },
+    {
+        title: 'Tab 7',
+        name: '7',
+        content: 'Tab 7 content',
+    },
+    {
+        title: 'Tab 8',
+        name: '8',
+        content: 'Tab 6 content',
+    },
+    {
+        title: 'Tab 9',
+        name: '9',
+        content: 'Tab 9 content',
+    },
+])
+
+const addTab = (targetName) => {
+    const newTabName = `${++tabIndex}`
+    editableTabs.value.push({
+        title: 'New Tab',
+        name: newTabName,
+        content: 'New Tab content',
+    })
+    editableTabsValue.value = newTabName
+}
+const removeTab = (targetName) => {
+    const tabs = editableTabs.value
+    let activeName = editableTabsValue.value
+    if (activeName === targetName) {
+        tabs.forEach((tab, index) => {
+            if (tab.name === targetName) {
+                const nextTab = tabs[index + 1] || tabs[index - 1]
+                if (nextTab) {
+                    activeName = nextTab.name
+                }
+            }
+        })
+    }
+
+    editableTabsValue.value = activeName
+    editableTabs.value = tabs.filter((tab) => tab.name !== targetName)
+}
+</script>
+<style scoped>
+/* 固定 */
+.f-tag-list {
+    @apply fixed bg-gray-100 flex items-center px-2;
+    top: 64px;
+    right: 0;
+    height: 44px;
+    z-index: 100;
+}
+
+.tag-btn{
+    @apply bg-white rounded ml-auto flex items-center justify-center px-2;
+    height: 32px;
+}
+:deep(.el-tabs__header){
+    border: 0!important;
+    @apply mb-0;
+}
+:deep(.el-tabs__nav){
+    border: 0!important;
+}
+:deep(.el-tabs__item){
+    border: 0!important;
+    height: 32px;
+    line-height: 32px;
+    @apply bg-white mx-1 rounded;
+}
+:deep(.el-tabs__nav-next),:deep(.el-tabs__nav-prev){
+    line-height: 32px;
+    height: 32px;
+}
+:deep(.is-disabled){
+    cursor: not-allowed;
+    @apply text-gray-300;
+}
+</style>
